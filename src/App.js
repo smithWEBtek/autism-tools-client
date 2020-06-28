bddebdef979b2fa54459d9daa22a622fbd4a283f
github username <github email address>
2020-06-27 22:56:04 -0400

    Style main navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import MainNav from "./UI/MainNav/MainNav";
+
 import Users from "./Components/User/Users";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        autism-tools.org
+        <MainNav />
         <Users />
       </header>
     </div>
