58d45c993750d0a4bac3e7ad8361a50c18e846f0
brad@smithwebtek.com <brad@smithwebtek.com>
2021-02-13 11:27:53 -0500

    Experiment and update ErrorBoundary

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -10,8 +10,14 @@ class ErrorBoundary extends React.Component {
   }
 
   componentDidCatch(error, errorInfo) {
+    // https://jonbellah.com/articles/react-error-boundary/
     // You can also log the error to an error reporting service
+    // First, in componentDidCatch, you’ll notice the pseudo-method 
+    // logErrorToMyService(). 
+    // If you’re using a third-party error tracking service, 
+    // like Rollbar or Raygun, this is an excellent place to log those errors.
     logErrorToMyService(error, errorInfo);
+    console.log('ERROR BOUNDARY: ', error, errorInfo)
   }
 
   render() {
@@ -19,7 +25,8 @@ class ErrorBoundary extends React.Component {
       // You can render any custom fallback UI
       return <h1>Something went wrong.</h1>;
     }
-
+    // return this.props.children is 
+    // everything between our <ErrorBoundary></ErrorBoundary> wrapper.
     return this.props.children;
   }
 }
